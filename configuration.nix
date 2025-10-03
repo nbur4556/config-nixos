c2bf42c9a5c19b76c6b70f069e978da84b915ca3
Nick Burt <nbur4556@gmail.com>
2025-10-02 21:26:54 -0500

    install basic console and editor packages

--- configuration.nix
+++ configuration.nix
@@ -63,8 +63,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    git
+    tmux
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
