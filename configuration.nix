68e431fc330f16a0e6861779f389bb7510230381
Nick Burt <nbur4556@gmail.com>
2025-10-02 23:09:02 -0500

    setup required neovim dependency packages

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,10 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     tmux
+    neovim
+    ripgrep
+    fzf
+    gcc
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
