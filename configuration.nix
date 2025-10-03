cdcd65f01c767af2b457b7a4f764957fd9790368
Nick Burt <nbur4556@gmail.com>
2025-10-02 21:57:38 -0500

    install and enable hyprland

--- configuration.nix
+++ configuration.nix
@@ -66,7 +66,10 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     tmux
+    hyprland
   ];
+
+  programs.hyprland.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
